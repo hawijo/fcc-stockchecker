5273e87110f9b9bb88e345c76d88c35db028c1aa
Joshua <joshuahawi24@gmail.com>
2025-01-31 12:17:23 +0300

    Updated api step 1 done

--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,23 @@
 'use strict';
 
+
 let mongodb = require('mongodb')
 let mongoose = require('mongoose')
+var expect = require('chai').expect;
 
 
 module.exports = function (app) {
+
+  let uri = 'mongodb+srv://joshuahawi24:' + process.env.PW + '@w3-tutorial.q90bo.mongodb.net/?retryWrites=true&w=majority&appName=W3-Tutorial'
+
+	mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+
+  let stockSchema = new mongoose.Schema({
+    name: {type: String, required: true},
+    likes: {type: Number, default: 0},
+    ips: [String]
+  })
+  let Stock = mongoose.model('Stock', stockSchema)
 
   app.route('/api/stock-prices')
     .get(function (req, res){
