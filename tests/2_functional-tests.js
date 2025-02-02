9cee810b153720b8cecf49b9147a4d47a3848d5c
Joshua <joshuahawi24@gmail.com>
2025-02-02 14:51:02 +0300

    functional test.js

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -1,23 +1,56 @@
-const chaiHttp = require('chai-http');
-const chai = require('chai');
-const assert = chai.assert;
-const server = require('../server');
+/*
+*
+*
+*       FILL IN EACH FUNCTIONAL TEST BELOW COMPLETELY
+*       -----[Keep the tests in the same order!]-----
+*       (if additional are added, keep them at the very end!)
+*/
+
+var chaiHttp = require('chai-http');
+var chai = require('chai');
+var assert = chai.assert;
+var server = require('../server');
 
 chai.use(chaiHttp);
 
 suite('Functional Tests', function() {
-    test('1 stock', function(done) {
+    
+    suite('GET /api/stock-prices => stockData object', function() {
+      
+      test('1 stock', function(done) {
+       chai.request(server)
+        .get('/api/stock-prices')
+        .query({stock: 'goog'})
+        .end(function(err, res){
+          assert.equal(res.body['stockData']['stock'], 'goog')
+          assert.isNotNull(res.body['stockData']['price'])
+           assert.isNotNull(res.body['stockData']['likes'])
+          done();
+        });
+      });
+      
+      test('1 stock with like', function(done) {
         chai.request(server)
-         .get('/api/stock-prices')
-         .query({stock: 'goog'})
-         .end(function(err, res){
-           assert.equal(res.body['stockData']['stock'], 'goog')
-           assert.isNotNull(res.body['stockData']['price'])
-            assert.isNotNull(res.body['stockData']['likes'])
-           done();
-         });
-       });
-       test('2 stocks', function(done) {
+        .get('/api/stock-prices')
+        .query({stock: 'aapl', like: true})
+        .end(function(err, res){
+          assert.equal(res.body['stockData']['stock'], 'aapl')
+          assert.equal(res.body['stockData']['likes'], 1)
+          done();
+        });
+      });
+      
+      test('1 stock with like again (ensure likes arent double counted)', function(done) {
+        chai.request(server)
+        .get('/api/stock-prices')
+        .query({stock: 'aapl', like: true})
+        .end(function(err, res){
+          assert.equal(res.body, 'Error: Only 1 Like per IP Allowed')
+          done()
+        });
+      });
+      
+      test('2 stocks', function(done) {
         chai.request(server)
         .get('/api/stock-prices')
         .query({stock: ['aapl', 'amzn']})
@@ -68,4 +101,7 @@ suite('Functional Tests', function() {
           done()
         });
       });
+      
+    });
+
 });
